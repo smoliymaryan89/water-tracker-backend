8c2ab05f67aade97e3883bc0cf3508e1609922d1
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-12 15:00:08 +0200

    created user model and auth

---
+++ models/Users.js
@@ -0,0 +1,70 @@
+import Joi from "joi";
+import { Schema, model } from "mongoose";
+import { handleSaveError, runValidators } from "./hooks.js";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const sexList = ["girl", "man"];
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      maxlength: 64,
+      required: true,
+    },
+    oldPassword: {
+      type: String,
+      minlength: 8,
+      maxlength: 64,
+    },
+    sex: {
+      type: String,
+      enum: sexList,
+      default: sexList[0],
+    },
+    avatarURL: {
+      type: String,
+    },
+    token: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", runValidators);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+export const userSignupSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).max(64).required(),
+});
+
+export const userSigninSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).max(64).required(),
+});
+
+export const userRefreshTokenSchema = Joi.object({
+  refreshToken: Joi.string().required(),
+});
+
+const User = model("user", userSchema);
+
+export default User;
