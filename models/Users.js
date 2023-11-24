a52ce54c1a8128bf1da4b23749564bd4906e745c
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-24 13:49:01 +0200

    fixed

--- models/Users.js
+++ models/Users.js
@@ -91,7 +91,7 @@ export const updateUserInfoSchema = Joi.object({
 });
 
 export const userWaterRateSchema = Joi.object({
-  waterRate: Joi.integer().min(0).max(15000).required(),
+  waterRate: Joi.number().min(0).max(15000).required(),
 });
 
 const User = model("user", userSchema);
